f308e647abd711c17635c118d4af57296c72fc81
Adam Klein <adam3207@gmail.com>
2026-07-22 03:32:18 +0000

    print

--- src/models/oloop.py
+++ src/models/oloop.py
@@ -223,6 +223,7 @@ class FastWeight(nn.Module):
 
         s = self.get_s()
 
+        print(s.shape, x.shape, flush=True)
         y = torch.einsum("boi,bli->blo", s, x)
         y = FastWeightFunction.apply(x, y, self.momentum)
 
