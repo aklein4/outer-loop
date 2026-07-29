bc917c56e1a4308fa9c7326be34669a99ae1a584
Adam Klein <adam3207@gmail.com>
2026-07-29 22:54:09 +0000

    improve newton_schulz

--- src/utils/torch_utils.py
+++ src/utils/torch_utils.py
@@ -293,7 +293,7 @@ def safe_finite(x: torch.Tensor, safe=False) -> torch.Tensor:
         return torch.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
 
 
-def newton_schulz(G, steps=5, eps=1e-7):
+def newton_schulz(G, steps=5, eps=1e-7, polar=False, safety=None):
     """
     Perform spectral whitening on G using Newton-Schulz iteration.
 
@@ -306,41 +306,46 @@ def newton_schulz(G, steps=5, eps=1e-7):
     Returns:
         torch.Tensor: Spectrally whitened tensor of shape [n, m].
     """
-    assert G.ndim >= 2 
+    assert G.ndim >= 2
+
+    polar_coeffs = [
+        (8.28721201814563, -23.595886519098837, 17.300387312530933),
+        (4.107059111542203, -2.9478499167379106, 0.5448431082926601),
+        (3.9486908534822946, -2.908902115962949, 0.5518191394370137),
+        (3.3184196573706015, -2.488488024314874, 0.51004894012372),
+        (2.300652019954817, -1.6689039845747493, 0.4188073119525673),
+        (1.891301407787398, -1.2679958271945868, 0.37680408948524835),
+        (1.8750014808534479, -1.2500016453999487, 0.3750001645474248),
+        (1.875, -1.25, 0.375),
+    ]
+    standard_coeffs = [
+        (3.4445, -4.7750,  2.0315)
+    ] * 8
     
+    if safety is None:
+        safety = 0.5 if polar else 1.0
+    coeffs = polar_coeffs if polar else standard_coeffs
+
     X = G
     if G.size(-2) > G.size(-1):
-        X = X.transpose(-2, -1)
+        X = X.mT
 
     # Ensure spectral norm is at most 1
     X_f = X.float()
     X = (X_f / (X_f.norm(dim=(-2, -1), keepdim=True) + eps)).to(X.dtype)
 
     # Perform the NS iterations
-    if constants.XLA_AVAILABLE and False:
-        ts = torch.arange(steps, device=X.device)
-        X, _ = scan(
-            _newton_schulz_inner, X, ts,
-            # is_fn_pure=True
-        )
-    else:
-        for t in range(steps):
-            X, _ = _newton_schulz_inner(X, t)
+    for t in range(steps):
+        a, b, c = coeffs[t]
+
+        A = X @ X.mT
+        B = b * A + c * A @ A
+        X = a * X + B @ X
         
     if G.size(-2) > G.size(-1):
-        X = X.transpose(-2, -1)
+        X = X.mT
 
     return X
-
-
-def _newton_schulz_inner(X, t):
-    a, b, c = (3.4445, -4.7750,  2.0315)
-
-    A = X @ X.transpose(-2, -1)
-    B = b * A + c * A @ A
-    X = a * X + B @ X
-
-    return X, X
 
 
 _cuda_newton_schulz = None
